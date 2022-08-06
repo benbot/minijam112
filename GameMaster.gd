d1c785759cb7e1e272616ed6bda241c0254c9506
benbot <bbotwin@atlassian.com>
2022-08-05 22:39:06 -0400

    2nd commit

--- GameMaster.gd
+++ GameMaster.gd
@@ -1,16 +1,27 @@
 extends Node
 
-var current_state = {
-	"input" = func(e):
-		if e.is_action("1"):
-			pass
-		elif e.is_action("2"):
-			pass
-		elif e.is_action("3"):
-			pass
+@onready var player: Node3D = get_tree().get_first_node_in_group("player")
+@export var rot_speed = 20
+
+var overworld_state := {
+	"p_update": func(delta):
+		var rot_amt = Input.get_action_strength("1") - Input.get_action_strength("3")
+		player.rotate_y(deg2rad(rot_speed * delta * rot_amt))
 }
+
+var current_state = overworld_state
 
 func _input(event: InputEvent) -> void:
 	var input = current_state.get("input")
 	if input is Callable:
 		input.call(event)
+
+func _process(delta):
+	var update = current_state.get("update")
+	if update is Callable:
+		update.call(delta)
+
+func _physics_process(delta):
+	var p_update = current_state.get("p_update")
+	if p_update is Callable:
+		p_update.call(delta)
