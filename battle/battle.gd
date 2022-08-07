c153df5941d94c27d537410967d57655f102279c
Ben Botwin <benj@minbot.win>
2022-08-07 13:15:49 -0400

    added a ton of assets

--- battle/battle.gd
+++ battle/battle.gd
@@ -6,9 +6,13 @@ extends Node2D
 @onready var startpoint: Node2D = $start
 @onready var endpoint: Node2D = $end
 
+@onready var player_sprite: AnimatedSprite2D = $playerSprite
+@onready var enemy_sprite: AnimatedSprite2D = $enemySprite
+
 @onready var tween := get_tree().create_tween().bind_node(self)
 
 var target_overlap := false
+var can_attack = true
 
 func _set_target_overlap(_body: CollisionObject2D, value: bool) -> void:
 	target_overlap = value
@@ -22,5 +26,19 @@ func _ready():
 	tween.tween_property(player, "global_position", startpoint.global_position, speed)
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("2"):
-		print(target_overlap)
+	if Input.is_action_just_pressed("2") and can_attack:
+		if target_overlap:
+			player_sprite.play("ATTACK")
+			enemy_sprite.play("HIT")
+			await enemy_sprite.animation_finished
+			enemy_sprite.play("IDLE")
+			player_sprite.play("IDLE")
+			can_attack = true
+		else:
+			enemy_sprite.play("ATTACK")
+			player_sprite.play("HIT")
+			await player_sprite.animation_finished
+			enemy_sprite.play("IDLE")
+			player_sprite.play("IDLE")
+			can_attack = true
+
